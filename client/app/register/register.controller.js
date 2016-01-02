9439623129725e9bb0600d8c56bb3a7664833da0
Pranu Sarna <pranusarna@gmail.com>
2016-01-02 12:32:51 +0530

    form functionin ends, styling begins

--- client/app/register/register.controller.js
+++ client/app/register/register.controller.js
@@ -1,6 +1,15 @@
 'use strict';
 
 angular.module('ecophoriaApp')
-  .controller('RegisterCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+    .controller('RegisterCtrl', function ($scope, $http) {
+
+        $scope.submitInfo = function (participant) {
+            $http.post('/api/participants', participant)
+                .success(function (res) {
+                    console.log(res);
+                })
+                .error(function (res) {
+                    console.log(res);
+                })
+        }
+    });
