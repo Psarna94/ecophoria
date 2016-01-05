78d793ed357c880bd75ec905f0deb29f90776aba
Pranu Sarna <pranusarna@gmail.com>
2016-01-05 16:49:15 +0530

    made the events grid responive

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,31 +1,45 @@
 'use strict';
 
-(function() {
 
-class MainController {
-
-  constructor($http) {
-    this.$http = $http;
-    this.awesomeThings = [];
-
-    $http.get('/api/things').then(response => {
-      this.awesomeThings = response.data;
-    });
-  }
-
-  addThing() {
-    if (this.newThing) {
-      this.$http.post('/api/things', { name: this.newThing });
-      this.newThing = '';
-    }
-  }
-
-  deleteThing(thing) {
-    this.$http.delete('/api/things/' + thing._id);
-  }
-}
 
 angular.module('ecophoriaApp')
-  .controller('MainController', MainController);
+  .controller('MainController', function($scope, $location, $anchorScroll){
+        $scope.events = [
+            {
+                name:"Monopoly"
+            },
+            {
+                name:"Monopoly"
+            },
+            {
+                name:"Monopoly"
+            },
+            {
+                name:"Monopoly"
+            },
+            {
+                name:"Monopoly"
+            },
+            {
+                name:"Monopoly"
+            },
+            {
+                name:"Monopoly"
+            },
+            {
+                name:"Monopoly"
+            },
+            {
+                name:"Monopoly"
+            }
+        ];
 
-})();
+        $scope.scrollToEvents = function(){
+            $location.hash('events');
+
+      // call $anchorScroll()
+      $anchorScroll();
+        }
+    });
+
+
